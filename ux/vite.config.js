5658603c611869130baffb97305290bb64b805c3
Robert Woodley <githubcommit.parachute821@passmail.net>
2025-10-24 11:33:05 -0500

    GH-1-WIP GitHub pages.

--- ux/vite.config.js
+++ ux/vite.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Set base path for GitHub Pages
+  // For repo at github.com/username/GreenAcres.UX, it will be served at username.github.io/GreenAcres.UX/
+  // Change to '/' if using a custom domain or deploying to username.github.io
+  base: '/GreenAcres.UX/',
   server: {
     proxy: {
       '/api': {
