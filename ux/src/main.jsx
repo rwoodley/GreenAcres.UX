0b5943c6567d1f69b82903227bf64c53a8f6307c
Robert Woodley <githubcommit.parachute821@passmail.net>
2025-10-24 13:19:11 -0500

    GH-1-WIP. It was redirecting to rwoodley.org on successful login.

--- ux/src/main.jsx
+++ ux/src/main.jsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')).render(
     domain={domain}
     clientId={clientId}
     authorizationParams={{
-      redirect_uri: window.location.origin,
+      redirect_uri: window.location.origin + import.meta.env.BASE_URL,
       audience: audience
     }}
   >
